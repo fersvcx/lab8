6bda03de9cf1542adeb57a05a617171deabce65c
fersvcx <rionhuy@gmail.com>
2024-04-11 12:09:52 +0700

    first commit

---
+++ Assets/Scripts/Scripts/CircleController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    private int direction = 1;
+    private float moveSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(0f,direction,0f);
+        transform.Translate(movement*moveSpeed *Time.deltaTime);
+        if(transform.position.y>1.5f || transform.position.y < -2.5f)
+        {
+            direction *= -1;
+        }
+    }
+
+    public int scoreValue = 10;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            // Tăng điểm số khi viên đạn va chạm vào mục tiêu
+          
+
+            // Biến mục tiêu và viên đạn biến mất
+            Destroy(gameObject); // Mục tiêu
+            Destroy(collision.gameObject); // Viên đạn
+        }
+    }
+}
---
+++ Assets/Scripts/Scripts/TargetController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TargetController : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            // Tăng điểm số khi viên đạn va chạm vào mục tiêu
+          
+
+            // Biến mục tiêu và viên đạn biến mất
+            Destroy(gameObject); // Mục tiêu
+            Destroy(collision.gameObject); // Viên đạn
+        }
+    }
+}
